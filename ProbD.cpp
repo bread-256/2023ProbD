37b977f8312ac85fb0d2965a0a764134b4686342
Adrian Tiu <adriantiu2010@gmail.com>
2026-03-18 22:35:58 +0800

    fixed row creation logic need to reroute to find optimum c

--- ProbD.cpp
+++ ProbD.cpp
@@ -5,29 +5,30 @@ void solve(){
     int count = 0;
     int c;
     std::string s;
-
     std::cin>>c;
     std::cin>>s;
 
     int sizeS = sizeof(s)/sizeof(s[0]);
-    int r = (sizeS+(c-(sizeS%c)))/c;
+    int lastRow = sizeS%c;
+    int r = (sizeS+c-1)/c;
 
-    bool sPrime[c][r];
-    
+    int sPrime[r][c];
+
     for(int i = 0; i<c; i++){
-	for(int j = 0; j<r; j++){
-            if((i+1)*(j+1)>sizeS){ std::cout<<"\nextra\n"; /*&sPrime[i][j] = nullprt*/;
-	    } else if(s[i+j*c]=='1'){ sPrime[i][j] = true;
-            } else sPrime[i][j] = false;
+        for(int j = 0; j<r; j++){
+                if(((c*j)+i)>sizeS-1){ sPrime[j][i] = -1;
+                } else if(s[i+j*c]=='1'){ sPrime[j][i] = true;
+                } else if(s[i+j*c]=='0'){ sPrime[j][i] = false;
+            }
         }
     }
-
-    for(int i = 0; i+3<c; i++){
-        for(int j = 0; j+3<r; j+=c){
-            if(sPrime[i][j] && !sPrime[i+1][j] && !sPrime[i+2][j] && !sPrime[i+3][j] &&
-            !sPrime[i][j+1] && !sPrime[i+1][j+1] && sPrime[i+2][j+1] && sPrime[i+3][j+1] &&
-            !sPrime[i][j+2] && !sPrime[i+1][j+2] && !sPrime[i+2][j+2] && !sPrime[i+3][j+2] &&
-            sPrime[i][j+3] && !sPrime[i+1][j+3] && sPrime[i+2][j+3] && !sPrime[i+3][j+3]) count+=1;
+    
+    for(int j = 0; j+3<r; j++){
+        for(int i = 0; i+3<c; i++){
+            if(sPrime[j][i] && !sPrime[j][i+1] && !sPrime[j][i+2] && !sPrime[j][i+3] &&
+            !sPrime[j+1][i] && !sPrime[j+1][i+1] && sPrime[j+1][i+2] && sPrime[j+1][i+3] &&
+            !sPrime[j+2][i] && !sPrime[j+2][i+1] && !sPrime[j+2][i+2] && !sPrime[j+2][i+3] &&
+            sPrime[j+3][i] && !sPrime[j+3][i+1] && sPrime[j+3][i+2] && !sPrime[j+3][i+3]) count+=1;
         }
     }
 
